afef9dbf9195867624fc386feb9523b0e3730338
Haymanot Asmare <hasmare463@gmail.com>
2025-12-01 23:13:15 -0800

    feat: implement Layout component for structured page layout

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from '../common/Header';
+import Footer from '../common/Footer';
+import ThemeToggle from '../common/ThemeToggle';
+import Background from './Background';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-300">
+      <Background />
+      <ThemeToggle />
+      
+      <div className="relative z-10">
+        <Header />
+        <main className="pt-16">
+          {children}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
